4e1ef0dffcf107dbe28d5399aa1dbeec9c86c697
CodyTheDoer <me@codythedoer.com>
2024-10-15 18:17:34 -0400

    Updated TODO and planning for future

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
+use bevy::input::mouse::MouseMotion;
+use bevy::pbr::{CascadeShadowConfigBuilder};
 use bevy::prelude::*;
+use std::f32::consts::*;
 
 #[derive(Debug, Component)]
-struct GlbComponent;
+pub struct GlbComponent;
 
 #[derive(Debug, Component)]
-struct Player;
+pub struct Player;
 
 #[derive(Debug, Component)]
-struct WorldModelCamera;
+pub struct WorldModelCamera;
 
 pub fn setup_glb(
     mut commands: Commands, 
     asset_server: Res<AssetServer>, 
-    glb_path: &str
+    glb_path: String,
 ) {
     commands.spawn((
         SceneBundle {
-            scene: asset_server.load(glb_path), // Load the scene from GLB file
+            // Load the scene from GLB file
+            scene: asset_server.load(glb_path),
             ..default()
         },
         GlbComponent,  // Tag it for raycasting detection
@@ -69,3 +73,107 @@ pub fn spawn_lights(
     ));
 }
 
+pub fn adjust_player_camera(
+    mut mouse_motion: EventReader<MouseMotion>,
+    mut player: Query<&mut Transform, With<Player>>,
+) {
+    let mut transform = player.single_mut();
+    for motion in mouse_motion.read() {
+        let yaw = -motion.delta.x * 0.003;
+        let pitch = motion.delta.y * 0.002;
+        // Order of rotations is important, see <https://gamedev.stackexchange.com/a/136175/103059>
+        transform.rotate_y(yaw);
+        transform.rotate_local_x(pitch);
+    }
+}
+
+pub fn change_fov(
+    input: Res<ButtonInput<KeyCode>>,
+    mut world_model_projection: Query<&mut Projection, With<WorldModelCamera>>
+) {
+    let mut projection = world_model_projection.single_mut();
+    let Projection::Perspective(ref mut perspective) = projection.as_mut() else {
+        unreachable!(
+            "The `Projection` component was explicitly built with `Projection::Perspective`"
+        );
+    };
+
+    if input.pressed(KeyCode::ArrowUp) {
+        perspective.fov -= 1.0_f32.to_radians();
+        perspective.fov = perspective.fov.max(20.0_f32.to_radians());
+    }
+    if input.pressed(KeyCode::ArrowDown) {
+        perspective.fov += 1.0_f32.to_radians();
+        perspective.fov = perspective.fov.min(160.0_f32.to_radians());
+    }
+}
+
+pub fn draw_cursor(
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    glb_component_query: Query<&GlobalTransform, With<GlbComponent>>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let glb_component = glb_component_query.single();
+
+    let Some(cursor_position) = windows.single().cursor_position() else {
+        return;
+    };
+
+    // Calculate a ray pointing from the camera into the world based on the cursor's position.
+    let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+        return;
+    };
+
+    // Calculate if and where the ray is hitting the glb_component.
+    let Some(distance) =
+        ray.intersect_plane(glb_component.translation(), InfinitePlane3d::new(glb_component.up()))
+    else {
+        return;
+    };
+    let point = ray.get_point(distance);
+
+    // Draw a circle just above the glb_component at that position.
+    gizmos.circle(point + glb_component.up() * 0.01, glb_component.up(), 0.2, Color::WHITE);
+}
+
+pub fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in &mut query {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.elapsed_seconds() * PI / 5.0,
+            -FRAC_PI_4,
+        );
+    }
+}
+
+pub fn spawn_text(mut commands: Commands) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                bottom: Val::Px(12.0),
+                left: Val::Px(12.0),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                concat!(
+                    "Move the camera with your mouse holding right click to enable movement.\n",
+                    "Press arrow up to decrease the FOV of the world model.\n",
+                    "Press arrow down to increase the FOV of the world model."
+                ),
+                TextStyle {
+                    font_size: 25.0,
+                    ..default()
+                },
+            ));
+        });
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+/*
+TODO:
+Figure out how to spawn text on the screen component of the calculator
+Build iterator to extract triangles from GLB files
+Setup wrti logic in scene_handler_library and integrate the ray tracer
+Build left click ray interchange test and see if I can just hit the triangles
+Figure out how to load multiple gbl files into the scene_library_handler
+*/
+
+use scene_handler_library::{
+    setup_glb, spawn_view_model, spawn_lights, animate_light_direction,
+    draw_cursor, spawn_text, change_fov, adjust_player_camera,
+}; 
+
+use wrti_library::watertight_ray_triangle_intersection;
+
+use glam::Vec3;
+
+use bevy::input::common_conditions::*;
+use bevy::pbr::DirectionalLightShadowMap;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .insert_resource(DirectionalLightShadowMap { size: 4096 })
+    .add_systems(
+        Startup,
+        (   
+            wrti_test,
+            spawn_view_model,
+            spawn_lights,
+            spawn_text,
+            |commands: Commands, asset_server: Res<AssetServer>| setup_glb(commands, asset_server, "cube.glb#Scene0".to_string()),
+        ),
+    )
+    .add_systems(
+        Update, 
+        (
+            adjust_player_camera.run_if(input_pressed(MouseButton::Right)),
+            draw_cursor,
+            change_fov,
+            animate_light_direction,
+        ),
+    )
+    .run();
+}
+
+fn wrti_test() {
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let direction = Vec3::new(0.0, 0.0, 1.0);
+    let triangle = (
+        Vec3::new(1.0, 0.0, 5.0),
+        Vec3::new(-1.0, 1.0, 5.0),
+        Vec3::new(-1.0, -1.0, 5.0),
+    );
+    let backface_culling = false;
+
+    if let Some(hit) = watertight_ray_triangle_intersection(origin, direction, triangle, backface_culling) {
+        // call results for testing as needed
+        println!("Intersection found at t = {}", hit.t());
+        
+        // call all the results
+        let (t, u, v, w) = hit.as_tuple();
+        println!("Hit Breakdown: t: {}, u: {}, v: {}, w: {}", t, u, v, w)
+    } else {
+        println!("No intersection found");
+    }
+}
